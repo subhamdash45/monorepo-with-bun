0096dff9bcf219a2e4bc1eaa05af2b9c621f2177
Subham Dash <mrsubhamdash1998@gmail.com>
2025-03-22 23:14:38 +0530

    monorepo with bun setup

---
+++ apps/ws/index.ts
@@ -0,0 +1,23 @@
+import { prismaClient } from "db/client";
+
+Bun.serve({
+  port: 8081,
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    message(ws: { send: (arg0: any) => void }, message: any) {
+      prismaClient.user.create({
+        data: {
+          username: Math.random().toString(),
+          password: Math.random().toString(),
+        },
+      });
+      ws.send(message);
+    },
+  },
+});
